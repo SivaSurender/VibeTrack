45f79930c36bc89501026800c65a7a274f64c58b
SivaSurender <e.surender14@gmail.com>
2023-08-20 16:39:45 +0530

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import Products from "./pages/Products";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
